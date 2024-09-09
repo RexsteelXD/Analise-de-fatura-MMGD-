0a445516439c18c0c9a2a362fe9a651230286ad7
RexsteelXD <gladson_carlos@hotmail.com>
2024-09-09 14:53:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+    {titulo: "Fatura MMGD",
+        descricao: "Fatura de Micro e Mini Geração Distribuída (MMGD): Um Guia Completo. A fatura de uma unidade consumidora com geração distribuída, como sistemas fotovoltaicos, apresenta particularidades em relação às faturas tradicionais. Isso se deve ao fato de que, além de consumir energia da rede, esses consumidores também podem injetar energia nela, gerando créditos.",
+        link: "https://www.edponline.com.br/entenda-sua-conta/index.html?estado=sp&publico=parasuacasa",
+        tags: "MMGD fatura conta de energia", 
+    },
+    
+    {
+        titulo: "TUSD",
+        descricao: "A Tarifa de Uso do Sistema de Distribuição (TUSD) é o valor cobrado pela prestação dos serviços de transporte e distribuição de energia elétrica. Ou seja, é a parcela da conta de luz que remunera as empresas responsáveis por levar a energia até os consumidores finais.",
+        link: "https://www.edponline.com.br/entenda-sua-conta/index.html?estado=sp&publico=parasuacasa",
+        tags:"tarifa distribuição", 
+    },
+    {
+
+        titulo: "TE",
+        descricao: "O que é: Representa o custo da energia elétrica consumida pelo cliente. É a parte da conta de luz que remunera a geração da energia elétrica.                                                                                                                                                      Como funciona: A TE varia de acordo com a faixa de consumo e a época do ano, podendo ter diferentes bandeiras tarifárias que refletem as condições de geração de energia no país.",
+        link: "https://www.edponline.com.br/entenda-sua-conta/index.html?estado=sp&publico=parasuacasa",
+        tags:"tarida de energia",
+    },
+    {
+        titulo : "TUSD Injetado",
+        descricao : "O que é: Quando você possui um sistema de geração própria (como painéis solares) e a energia gerada excede o seu consumo, o excedente é injetado na rede elétrica. A TUSD Injetado é a parte da TUSD referente a essa energia injetada.                                                                                                                                                      Como funciona: A TUSD Injetado é calculada de forma similar à TUSD convencional, mas se aplica à energia que você envia para a rede.",
+        link : "https://www.edponline.com.br/entenda-sua-conta/index.html?estado=sp&publico=parasuacasa",
+        tags:"tarifa de distruição injetado",
+    },
+    {
+        titulo : "TE Injetado",
+        descricao : "O que é: Assim como a TUSD Injetado, a TE Injetado se refere à energia que você gera e injeta na rede. No entanto, nesse caso, a cobrança é referente à Tarifa de Energia aplicada à energia injetada.                                                                                                                                                      Como funciona: A TE Injetado é calculada com base na energia injetada e na tarifa de energia vigente no momento da injeção.",
+        link : "https://www.edponline.com.br/entenda-sua-conta/index.html?estado=sp&publico=parasuacasa",
+        tags:"tarifa de energia injetado",
+    },
+];
+
